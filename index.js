d79e71f2a4049643664a4400bb533d1d0f302805
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-25 05:59:00 +0600

    save user to db and jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -24,12 +25,45 @@ async function run() {
     const categoriesCollection = client
       .db("resellxDB")
       .collection("categories");
+    const usersCollections = client.db("resellxDB").collection("users");
 
     // get all categories
     app.get("/categories", async (req, res) => {
       const query = {};
       const categories = await categoriesCollection.find(query).toArray();
       res.send(categories);
+    });
+
+    //save user to db
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+
+      const exists = await usersCollections.findOne(query);
+
+      if (exists) {
+        return res.send({ exists: true });
+      }
+
+      const result = await usersCollections.insertOne(user);
+
+      res.send(result);
+    });
+
+    //get jwt
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollections.findOne(query);
+
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "7h",
+        });
+        return res.send({ accessToken: token });
+      }
+
+      res.status(403).send({ accessToken: "" });
     });
   } finally {
   }
