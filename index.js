f96505e4d87cc8c678af5f5475d257b0036c0e49
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-25 03:03:17 +0600

    mongodb added with categories get api call

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+// mongodb config
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjvalws.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// mongodb run
+
+async function run() {
+  try {
+    const categoriesCollection = client
+      .db("resellxDB")
+      .collection("categories");
+
+    // get all categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
+    });
+  } finally {
+  }
+}
+
+run().catch(error => console.log(error));
+
 // home route
 app.get("/", (req, res) => {
   res.send("<h1>Resellx server is running</h1>");
